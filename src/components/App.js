6284c143654b459a23912b8947c37405644f479d
Chisato Murakami <kiwipom1031@gmail.com>
2020-09-28 16:06:54 -0700

    add Link to connect to Book component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import '../App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import BooksList from './BooksList';
+import Book from './Book';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; 
+
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/:id">
+          <Book />
+        </Route>
         <Route path="/">
           <BooksList />
         </Route>
       </Switch>
     </Router>
+    
     )
 }
 
--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,26 +1,39 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-
+import { Row, Col, Image } from "react-bootstrap";
 import booksData from "../books.json";
+import { Link } from "react-router-dom";
 
 const BooksList = () => {
   const booksObj = booksData.results.books;
+  const booksObjtop10 = booksObj.slice(0,10)
 
   return (
     <React.Fragment>
-      <Container>
-        <h1>BooksList</h1>
-        {booksObj.map((book) => {
-          return (
-            <h2 key={book.rank}>
-              {book.rank}: {book.title}
-            </h2>
-          );
-        })}
-        {/* <button type="button" className="nes-btn is-primary">
-          Button
-        </button> */}
-      </Container>
+      <h1>Top 10 Books</h1>
+      {booksObjtop10.map((book) => {
+        return (
+          <Link key={book.rank} to={`/${book.rank}`} style={{textDecoration: "none", color: "inherit"}}>
+          
+          <div className="books">
+            <Row>
+              <Col xs={12} md={4} lg={4}>
+                <Image
+                  src={book.book_image}
+                  rounded
+                  style={{ width: "165px", height: "250px" }}
+                />
+              </Col>
+              <Col xs={12} md={8} lg={8} className="mt-3">
+                <h4 key={book.rank}>
+                  {book.rank}: {book.title}
+                </h4>
+                <p>By {book.author}</p>
+              </Col>
+            </Row>
+          </div>
+          </Link>
+        );
+      })}
     </React.Fragment>
   );
 };
