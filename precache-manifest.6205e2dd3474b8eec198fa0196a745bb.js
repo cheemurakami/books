d82dedb79d98c64b3563245bf43ae8ad53fd4f3e
Chisato Murakami <kiwipom1031@gmail.com>
2020-09-30 17:09:34 -0700

    Updates

---
+++ precache-manifest.6205e2dd3474b8eec198fa0196a745bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea1b319902866feba976ea8f0996321",
+    "url": "/books/index.html"
+  },
+  {
+    "revision": "94429d0a108a22caa1fd",
+    "url": "/books/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b90fd4d7545eaf67166a",
+    "url": "/books/static/css/main.3f863600.chunk.css"
+  },
+  {
+    "revision": "94429d0a108a22caa1fd",
+    "url": "/books/static/js/2.bb6bfcab.chunk.js"
+  },
+  {
+    "revision": "b90fd4d7545eaf67166a",
+    "url": "/books/static/js/main.899546fc.chunk.js"
+  },
+  {
+    "revision": "c9d872b8dd591b078af5",
+    "url": "/books/static/js/runtime-main.bb3917c2.js"
+  }
+]);
